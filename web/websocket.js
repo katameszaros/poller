a43b4fd87f5b99f94f8fd1e256302cfe072a8950
Kata Meszaros <kata.meszaros92@gmail.com>
2017-08-04 14:50:20 +0200

    Added dynamically changing diagram

--- web/websocket.js
+++ web/websocket.js
@@ -148,6 +148,63 @@ function showPollDetails(pollId, choices) {
         quantity.innerHTML = choices[i].quantity;
         choice.appendChild(quantity);
     }
+    showChart(pollId, choices);
+}
+
+function showChart(pollId, choices){
+    var option = {
+        responsive: true
+    };
+
+    var labels = [];
+    for (var i = 0; i < choices.length; i++) {
+        labels[i] = choices[i].description;
+    }
+
+    var data = [];
+    for (var i = 0; i < choices.length; i++) {
+        data[i] = choices[i].quantity;
+    }
+
+
+    var ctx = document.getElementById("myChart").getContext('2d');
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '# of Votes',
+                data: data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
 }
 
 function showForm() {
